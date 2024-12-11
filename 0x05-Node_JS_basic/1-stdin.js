f2b0c18fd0471c4a976cc867a1b30471fc9a3940
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-12-11 12:30:27 +0300

    rectified the code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,10 @@ process.stdout.write("Welcome to ALX, what is your name?\n");
 
 process.stdin.on('data', (input) => {
     const name = input.toString().trim();
-    process.stdout.write(`Welcome ${name}!\n`);
+    process.stdout.write(`Your name is: ${name}\n`);
+});
+
+// Always trigger the end event when the input stream is closed
+process.stdin.on('end', () => {
     process.stdout.write("This important software is now closing\n");
-    process.exit();
 });
